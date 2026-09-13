a1fecd8251be227465313317277b0cd0fc5bf4d7
Anahi Perez <anahiperesdsm@gmail.com>
2026-09-12 21:52:28 -0300

    hecho el ejercicio 05

--- ejercicios/05-dip.ts
+++ ejercicios/05-dip.ts
@@ -1,16 +1,37 @@
-class EmailSender {
+interface Notifier {
+  send(to: string, message: string): void;
+}
+
+
+class EmailSender implements Notifier {
   send(to: string, message: string): void {
     console.log(`Correo para ${to}: ${message}`);
   }
 }
 
-class OrderService {
-  createOrder(customerEmail: string): void {
-    console.log("Pedido creado");
-
-    const emailSender = new EmailSender();
-    emailSender.send(customerEmail, "Tu pedido fue creado");
+class SmsSender implements Notifier { //aca copie la implementacion concretra de correo y la adapte a sms
+  send(to: string, message: string): void {
+    console.log(`sms para ${to} ${message}`);
   }
 }
 
-new OrderService().createOrder("ana@example.com");
+class OrderService {
+  constructor(private sender: Notifier){}
+
+  createOrder(pedido: string): void {
+    console.log("Pedido creado");
+    // const emailSender = new EmailSender();
+    // emailSender.send(customerEmail, "Tu pedido fue creado");
+    this.sender.send(pedido, "tu pedido fue creado")
+  }
+}
+
+//new OrderService().createOrder("ana@example.com");
+//const emailSender = new EmailSender();
+const orderService = new OrderService(new EmailSender());
+orderService.createOrder("ana@example.com");
+
+//const smsSender = new SmsSender();
+const orderService2 = new OrderService(new SmsSender());
+
+orderService2.createOrder("600123456");//aca probamos con sms
