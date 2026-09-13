e223100ee326a97ca16202a9141fec17b9d3a825
Anahi Perez <anahiperesdsm@gmail.com>
2026-09-12 21:28:01 -0300

    realizado ejercicio 03-lsp

--- ejercicios/03-lsp.ts
+++ ejercicios/03-lsp.ts
@@ -1,36 +1,31 @@
-class Rectangle {
-  constructor(protected width: number, protected height: number) {}
-
-  setWidth(width: number): void {
-    this.width = width;
+abstract class Shape { //shape debe expoiner operaciones que todas las figuras puedan aplicar
+  area(): number{
+  return 0;
   }
+}
 
-  setHeight(height: number): void {
-    this.height = height;
-  }
+class Rectangle implements Shape {
+  constructor(
+    private width: number,
+    private height: number //privdado para que no se modifique pero la misma clase si puede acceder
+  ) {}
 
   area(): number {
     return this.width * this.height;
   }
 }
 
-class Square extends Rectangle {
-  setWidth(width: number): void {
-    this.width = width;
-    this.height = width;
-  }
+class Square implements Shape {
+  constructor(private side: number) {} //lo mismo aca, en este caso recibe un solo valor porque el ancho y la altura en un cuadrado tienen el mismo valor, no como en un rectangulo
 
-  setHeight(height: number): void {
-    this.width = height;
-    this.height = height;
+  area(): number {
+    return this.side * this.side;
   }
 }
 
-function resizeRectangle(rectangle: Rectangle): void {
-  rectangle.setWidth(5);
-  rectangle.setHeight(10);
-  console.log(`Area esperada: 50. Area obtenida: ${rectangle.area()}`);
+function calculoArea(shape: Shape): void { //esta funcion es para realizar la operacion del area independientemente de si es un reactangulo o cuadrado, justamente para eos es shape
+  console.log(`area: ${shape.area()}`);
 }
 
-resizeRectangle(new Rectangle(1, 1));
-resizeRectangle(new Square(1, 1));
+calculoArea(new Rectangle(5, 10)); //imprime el area pero calcula el area del rectangulo instanciandolo y mandandole los valores correspondientes
+calculoArea(new Square(10)); //imprime el area pero del cuadrado, instanciando la clase y mandandole un solo valor ya que para calcular el area necesitan de un mismo valor de ancho * altura
